d2dbd0bd9ac70cf26b3644cba8f82253bc1ff2ce
MubasharAHMED <ahmedmubasharp@gmail.com>
2025-02-21 11:10:23 +0100

    Initialize projects

---
+++ chat/src/main/java/motivation/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package motivation.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
